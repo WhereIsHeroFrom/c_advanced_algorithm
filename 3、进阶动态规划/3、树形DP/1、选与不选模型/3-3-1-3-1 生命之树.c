10d4073bb506df4367454cceb693a574a7a0cfb6
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-21 13:12:15 +0800

    树形DP

---
+++ 3、进阶动态规划/3、树形DP/1、选与不选模型/3-3-1-3-1 生命之树.c
@@ -0,0 +1,96 @@
+//////////////////////////////////////////////////////////////////////
+///////////////////////简单树形DP模板（选or不选）///////////////////////
+//////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <string.h>
+
+#define type long long
+#define maxn 100010
+#define maxm 200010 // 每个边存储两次
+#define inf -1000000000000LL
+
+int n;
+// dp[i][0] 代表以 i 为根节点的子树，且根节点不选的最大（小）价值（或方案数）
+// dp[i][1] 代表以 i 为根节点的子树，且根节点  选的最大（小）价值（或方案数）
+type dp[maxn][2];
+type w[maxn];
+
+// 邻接表存储树
+int head[maxn];
+int edge[maxm];
+int next[maxm];
+int cnt;
+
+void add_edge(int a, int b) {
+    edge[cnt] = b;
+    next[cnt] = head[a];
+    head[a] = cnt++;
+}
+
+type max(type a, type b) {
+    return a > b ? a : b;
+}
+
+// 每个要求题目不同，需要做修改
+type TreeDPSimple_InitVal(int u, int isChoose) {
+    return w[u] * isChoose;
+}
+
+// 每个要求题目不同，需要做修改
+type TreeDPSimple_Opt(type curVal, int isChoose, type ncVal, type cVal) {
+    if(isChoose) {
+        return curVal + max(ncVal, cVal);
+    }
+    return 0;
+}
+
+// 模板代码，一般不做修改
+void TreeDPSimple_Init(int n) {
+    for(int i = 0; i <= n; ++i) {
+        dp[i][0] = dp[i][1] = inf;
+    }
+    memset(head, -1, sizeof(head));
+    cnt = 0;
+}
+
+// 模板代码，一般不做修改
+type TreeDPSimple_DFS(int u, int isChoose, int fat) {
+    if(dp[u][isChoose] != inf) {
+        return dp[u][isChoose];
+    }
+    type ans = TreeDPSimple_InitVal(u, isChoose);
+    for(int i = head[u]; i != -1; i = next[i]) {
+        int v = edge[i];
+        if(v == fat) continue;
+        type nc = TreeDPSimple_DFS(v, 0, u);
+        type  c = TreeDPSimple_DFS(v, 1, u);
+        ans = TreeDPSimple_Opt(ans, isChoose, nc, c);
+    }
+    return dp[u][isChoose] = ans;
+}
+
+//////////////////////////////////////////////////////////////////////
+///////////////////////简单树形DP模板（选or不选）///////////////////////
+//////////////////////////////////////////////////////////////////////
+
+int main() {
+    scanf("%d", &n);
+    TreeDPSimple_Init(n);
+    for(int i = 1; i <= n; ++i) {
+        scanf("%lld", &w[i]);
+    }
+    for(int i = 0; i < n - 1; ++i) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        add_edge(a, b);
+        add_edge(b, a);
+    }
+    type ans = inf;
+    for(int i = 1; i <= n; ++i) {
+        TreeDPSimple_DFS(i, 0, 0);
+        TreeDPSimple_DFS(i, 1, 0);
+        ans = max(ans, max(dp[i][0], dp[i][1]) );
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
---
+++ 3、进阶动态规划/3、树形DP/1、选与不选模型/3-3-1-3-3 没有上司的舞会.c
@@ -0,0 +1,95 @@
+//////////////////////////////////////////////////////////////////////
+///////////////////////简单树形DP模板（选or不选）///////////////////////
+//////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <string.h>
+
+#define type int
+#define maxn 5010
+#define maxm 10010 // 每个边存储两次
+#define inf 1000000000
+
+int n;
+// dp[i][0] 代表以 i 为根节点的子树，且根节点不选的最大（小）价值（或方案数）
+// dp[i][1] 代表以 i 为根节点的子树，且根节点  选的最大（小）价值（或方案数）
+type dp[maxn][2];
+type a[maxn];
+int fat[maxn];
+
+// 邻接表存储树
+int head[maxn];
+int edge[maxm];
+int next[maxm];
+int cnt;
+
+void add_edge(int a, int b) {
+    edge[cnt] = b;
+    next[cnt] = head[a];
+    head[a] = cnt++;
+}
+
+type max(type a, type b) {
+    return a > b ? a : b;
+}
+
+// 每个要求题目不同，需要做修改
+type TreeDPSimple_InitVal(int u, int isChoose) {
+    return a[u] * isChoose;
+}
+
+// 每个要求题目不同，需要做修改
+type TreeDPSimple_Opt(type curVal, int isChoose, type ncVal, type cVal) {
+    if(isChoose) {
+        return curVal + ncVal;
+    }
+    return curVal + max(ncVal, cVal);
+}
+
+// 模板代码，一般不做修改
+void TreeDPSimple_Init(int n) {
+    for(int i = 0; i <= n; ++i) {
+        dp[i][0] = dp[i][1] = inf;
+    }
+    memset(head, -1, sizeof(head));
+    cnt = 0;
+}
+
+// 模板代码，一般不做修改
+type TreeDPSimple_DFS(int u, int isChoose, int fat) {
+    if(dp[u][isChoose] != inf) {
+        return dp[u][isChoose];
+    }
+    type ans = TreeDPSimple_InitVal(u, isChoose);
+    for(int i = head[u]; i != -1; i = next[i]) {
+        int v = edge[i];
+        if(v == fat) continue;
+        type nc = TreeDPSimple_DFS(v, 0, u);
+        type  c = TreeDPSimple_DFS(v, 1, u);
+        ans = TreeDPSimple_Opt(ans, isChoose, nc, c);
+    }
+    return dp[u][isChoose] = ans;
+}
+
+//////////////////////////////////////////////////////////////////////
+///////////////////////简单树形DP模板（选or不选）///////////////////////
+//////////////////////////////////////////////////////////////////////
+
+int main() {
+    scanf("%d", &n);
+    TreeDPSimple_Init(n);
+    for(int i = 1; i <= n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    for(int i = 0; i < n-1; ++i) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        add_edge(x, y);
+        add_edge(y, x);
+    }
+    type ans = max(
+        TreeDPSimple_DFS(1, 1, 0),
+        TreeDPSimple_DFS(1, 0, 0)
+    );
+    printf("%d\n", ans);
+    return 0;
+}
