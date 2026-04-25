bd2409a4f0272d0c9a6c914beedf2266145e4864
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-25 20:26:38 +0800

    模板化完成

--- 5、图论算法/2、二分图最大匹配/5-2-3-2 谈恋爱.c
+++ 5、图论算法/2、二分图最大匹配/5-2-3-2 谈恋爱.c
@@ -1,86 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 
-/////////////////////////////////////////////////////////////
 //////////////////二分图最大匹配模板(邻接表) //////////////////
-/////////////////////////////////////////////////////////////
 // 顶点范围  左边的点[1, n]，右边的点[1, m]
 
-#define maxn 1010
-#define maxm 1010
-#define false 0
-#define true 1
-#define bool int
+#define maxn 510
+#define maxm 510
 
+typedef struct {
+    int graph[maxn][maxm];
+    int deg[maxn];
+    int pre[maxm];
+    int visit[maxm];
+    int n, m;
+} Bipartite;
 
-
-// 邻接表实现
-int head[maxn];
-int edge[maxn*maxm];
-int next[maxn*maxm];
-int cnt;
-
-int pre[maxm];             // 匈牙利算法找增广路时的pre数组
-bool visit[maxm];          // 匈牙利算法找增广路时的visit数组
-int n, m;                  // 左、右点集的数目
-
-// 初始化二分图
-void Hungarian_Initialize(int n_, int m_) {
-    n = n_;
-    m = m_;
-    memset(pre, -1, sizeof(pre));
-    for (int i = 1; i <= n; ++i) {
-        head[i] = -1;
-    }
-    cnt = 0;
+void Hungarian_Initialize(Bipartite *bp, int n, int m) {
+    bp->n = n;
+    bp->m = m;
+    memset(bp->deg, 0, sizeof(bp->deg));
+    memset(bp->pre, -1, sizeof(bp->pre));
 }
 
-void Hungarian_AddEdge(int u, int v) {
-    edge[cnt] = v;
-    next[cnt] = head[u];
-    head[u] = cnt++;
+void Hungarian_AddEdge(Bipartite *bp, int u, int v) {
+    bp->graph[u][bp->deg[u]++] = v;
 }
 
-bool Hungarian_findMatch(int u) {
-    for (int i = head[u]; i != -1; i = next[i]) {
-        int v = edge[i];
-        if (!visit[v]) {
-            visit[v] = true;
-            int vpre = pre[v];
-            pre[v] = u;
-            if (vpre == -1 || Hungarian_findMatch(vpre)) {
-                return true;
+int hungarian_findMatch(Bipartite *bp, int u) {
+    for (int i = 0; i < bp->deg[u]; i++) {
+        int v = bp->graph[u][i];
+        if (!bp->visit[v]) {
+            bp->visit[v] = 1;
+            if (bp->pre[v] == -1 || hungarian_findMatch(bp, bp->pre[v])) {
+                bp->pre[v] = u;
+                return 1;
             }
-            pre[v] = vpre;
         }
     }
-    return false;
+    return 0;
 }
 
-// 获取最大匹配数
-int Hungarian_GetMaxMatch() {
-    int cnt = 0;
-    for (int i = 1; i <= n; ++i) {
-        memset(visit, false, sizeof(visit));
-        if (Hungarian_findMatch(i)) {
-            ++cnt;
+int Hungarian_GetMaxMatch(Bipartite *bp) {
+    int res = 0;
+    for (int u = 1; u <= bp->n; u++) {
+        memset(bp->visit, 0, sizeof(bp->visit));
+        if (hungarian_findMatch(bp, u)) {
+            res++;
         }
     }
-    return cnt;
+    return res;
 }
-/////////////////////////////////////////////////////////////
+
 //////////////////二分图最大匹配模板(邻接表) //////////////////
-/////////////////////////////////////////////////////////////
 
 int main() {
     int n, m, k;
     scanf("%d %d %d", &n, &m, &k);
-    Hungarian_Initialize(n, m);
+    Bipartite bp;
+    Hungarian_Initialize(&bp, n, m);
     for(int i = 1; i <= k; ++i) {
         int x, y;
         scanf("%d %d", &x, &y);
-        Hungarian_AddEdge(x, y);
+        Hungarian_AddEdge(&bp, x, y);
     }
-    printf("%d\n", Hungarian_GetMaxMatch());
+    printf("%d\n", Hungarian_GetMaxMatch(&bp));
     return 0;
 }
