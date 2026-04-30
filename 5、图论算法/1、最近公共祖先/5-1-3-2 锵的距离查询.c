4f4c0a6c9bb4b366b856f20968deb2de5c498f61
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-30 14:16:57 +0800

    模板化完成

--- 5、图论算法/1、最近公共祖先/5-1-3-2 锵的距离查询.c
+++ 5、图论算法/1、最近公共祖先/5-1-3-2 锵的距离查询.c
@@ -1,87 +1,96 @@
 #include <stdio.h>
 #include <string.h>
 
+///////////////////////最近公共祖先(倍增算法)///////////////////////
 #define maxn 500010
 #define maxd 18
 #define dummyroot 0
 
-int f[maxn][maxd]; // f[i][j] 代表i号结点的第2^j个祖先
-int dep[maxn];     // dep[i] 代表i在这棵树上的深度
+typedef struct {
+    int f[maxn][maxd];   // f[i][j] 代表i号结点的第2^j个祖先
+    int dep[maxn];       // dep[i] 代表i在这棵树上的深度
 
-// 邻接表实现
-int head[maxn];
-int edge[maxn*2];
-int next[maxn*2];
-int cnt;
+    int head[maxn];
+    int edge[maxn * 2];
+    int next[maxn * 2];
+    int cnt;
 
-// 队列实现
-int q[maxn];
-int front, rear;
+    int q[maxn];
+    int front, rear;
+    int n;               // 树的总结点数
+} LCA;
 
-void LCA_Init(int n) {
-    for(int i = 1; i <= n; ++i) {
-        head[i] = -1;
+
+void addSingleEdge(LCA *lca, int u, int v) {
+    lca->edge[lca->cnt] = v;
+    lca->next[lca->cnt] = lca->head[u];
+    lca->head[u] = lca->cnt++;
+}
+
+void LCA_Initialize(LCA *lca, int n) {
+    lca->n = n;
+    for (int i = 1; i <= n; ++i) {
+        lca->head[i] = -1;
     }
-    memset(f, dummyroot, sizeof(f));
-    cnt = 0;
+    memset(lca->f, dummyroot, sizeof(lca->f));
+    lca->cnt = 0;
 }
 
-void LCA_AddEdge(int u, int v) {
-    edge[cnt] = v;
-    next[cnt] = head[u];
-    head[u] = cnt++;
-    edge[cnt] = u;
-    next[cnt] = head[v];
-    head[v] = cnt++;
+void LCA_AddEdge(LCA *lca, int u, int v) {
+    addSingleEdge(lca, u, v);
+    addSingleEdge(lca, v, u);
 }
 
-void LCA_PreProcess(int root) {
-    front = rear = 0;
-    f[root][0] = dummyroot;
-    dep[root] = 0;
-    q[rear++] = root;
-    while( front < rear ) {
-        int u = q[front++];
-        for(int i = 1; i < maxd; ++i) {
-            f[u][i] = f[ f[u][i-1] ][ i-1 ];
-            if(f[u][i] == dummyroot) break;
+void LCA_PreProcess(LCA *lca, int root) {
+    lca->front = lca->rear = 0;
+    lca->f[root][0] = dummyroot;
+    lca->dep[root] = 0;
+    lca->q[lca->rear++] = root;
+
+    while (lca->front < lca->rear) {
+        int u = lca->q[lca->front++];
+        for (int i = 1; i < maxd; ++i) {
+            lca->f[u][i] = lca->f[lca->f[u][i-1]][i-1];
+            if (lca->f[u][i] == dummyroot) break;
         }
-        for(int i = head[u]; i != -1; i = next[i]) {
-            int v = edge[i];
-            if(v == f[u][0]) {
+        for (int i = lca->head[u]; i != -1; i = lca->next[i]) {
+            int v = lca->edge[i];
+            if (v == lca->f[u][0]) {
                 continue;
             }
-            f[v][0] = u;
-            dep[v] = dep[u] + 1;
-            q[rear++] = v;
+            lca->f[v][0] = u;
+            lca->dep[v] = lca->dep[u] + 1;
+            lca->q[lca->rear++] = v;
         }
     }
 }
 
-int LCA_Get(int u, int v) {
-    // 确保 u的深度 >= v的深度
-    if(dep[u] < dep[v]) {
-        return LCA_Get(v, u);
+int LCA_Get(LCA *lca, int u, int v) {
+    // 1、确保 u的深度 >= v的深度
+    if (lca->dep[u] < lca->dep[v]) {
+        return LCA_Get(lca, v, u); 
     }
-    // 将 u 和 v 调整到同一深度
-    for(int i = maxd-1; i >= 0; --i) {
-        if( dep[u] - (1<<i) >= dep[v] ) {
-            u = f[u][i];
+    // 2、将 u 和 v 调整到同一深度
+    for (int i = maxd - 1; i >= 0; --i) {
+        if (lca->dep[u] - (1 << i) >= lca->dep[v]) {
+            u = lca->f[u][i];
         }
     }
-    if(u == v) {
+    if (u == v) {
         return u;
     }
-    // 让 u 和 v 同时网上进行倍增
-    for(int i = maxd - 1; i >= 0; --i) {
-        if( f[u][i] != f[v][i] ) {
-            u = f[u][i];
-            v = f[v][i];
+    // 3、让 u 和 v 同时往上进行倍增
+    for (int i = maxd - 1; i >= 0; --i) {
+        if (lca->f[u][i] != lca->f[v][i]) {
+            u = lca->f[u][i];
+            v = lca->f[v][i];
         }
     }
-    return f[u][0];
+    return lca->f[u][0];
 }
+///////////////////////最近公共祖先(倍增算法)///////////////////////
 
+LCA lca;
 /*
            1
           /
@@ -98,19 +107,18 @@ int LCA_Get(int u, int v) {
 int main() {
     int n, q;
     scanf("%d %d", &n, &q);
-    LCA_Init(n);
+    LCA_Initialize(&lca, n);
     for(int i = 0; i < n-1; ++i) {
         int x, y;
         scanf("%d %d", &x, &y);
-        LCA_AddEdge(x, y);
+        LCA_AddEdge(&lca, x, y);
     }
-    LCA_PreProcess(1);
+    LCA_PreProcess(&lca, 1);
     while(q--) {
         int x, y;
         scanf("%d %d", &x, &y);
-        int z = LCA_Get(x, y);
-        // 容斥原理
-        printf("%d\n", (dep[x] - dep[z]) + (dep[y] - dep[z]));
+        int z = LCA_Get(&lca, x, y);
+        printf("%d\n", (lca.dep[x] - lca.dep[z]) + (lca.dep[y] - lca.dep[z]));
     }
     return 0;
 }
--- 5、图论算法/1、最近公共祖先/5-1-3-3 零食采购.c
+++ 5、图论算法/1、最近公共祖先/5-1-3-3 零食采购.c
@@ -1,94 +1,107 @@
 #include <stdio.h>
 #include <string.h>
 
-#define maxn 100010
-#define maxd 21
+///////////////////////最近公共祖先(倍增算法)///////////////////////
+#define maxn 500010
+#define maxd 18
 #define dummyroot 0
 
-int f[maxn][maxd]; // f[i][j] 代表i号结点的第2^j个祖先
-int dep[maxn];     // dep[i] 代表i在这棵树上的深度
+typedef struct {
+    int f[maxn][maxd];   // f[i][j] 代表i号结点的第2^j个祖先
+    int dep[maxn];       // dep[i] 代表i在这棵树上的深度
 
-// 邻接表实现
-int head[maxn];
-int edge[maxn*4];
-int next[maxn*4];
-int cnt;
+    int head[maxn];
+    int edge[maxn * 2];
+    int next[maxn * 2];
+    int cnt;
 
-int w[maxn], fw[maxn][21];  // fw[i][j]代表从根结点到i结点，有多少个种类为j的零食
+    int q[maxn];
+    int front, rear;
+    int n;               // 树的总结点数
+
+    int w[maxn];         // 新增1：每个结点的零食种类
+    int fw[maxn][21];    // fw[i][j]代表从根结点到i结点，有多少个种类为j的零食
+
+} LCA;
 
 
-// 队列实现
-int q[maxn*4];
-int front, rear;
+void addSingleEdge(LCA *lca, int u, int v) {
+    lca->edge[lca->cnt] = v;
+    lca->next[lca->cnt] = lca->head[u];
+    lca->head[u] = lca->cnt++;
+}
 
-void LCA_Init(int n) {
-    for(int i = 1; i <= n; ++i) {
-        head[i] = -1;
+void LCA_Initialize(LCA *lca, int n) {
+    lca->n = n;
+    for (int i = 1; i <= n; ++i) {
+        lca->head[i] = -1;
     }
-    memset(f, dummyroot, sizeof(f));
-    cnt = 0;
+    memset(lca->f, dummyroot, sizeof(lca->f));
+    lca->cnt = 0;
 }
 
-void LCA_AddEdge(int u, int v) {
-    edge[cnt] = v;
-    next[cnt] = head[u];
-    head[u] = cnt++;
-    edge[cnt] = u;
-    next[cnt] = head[v];
-    head[v] = cnt++;
+void LCA_AddEdge(LCA *lca, int u, int v) {
+    addSingleEdge(lca, u, v);
+    addSingleEdge(lca, v, u);
 }
 
-void LCA_PreProcess(int root) {
-    front = rear = 0;
-    f[root][0] = dummyroot;
-    fw[root][ w[root] ] = 1;
-    dep[root] = 0;
-    q[rear++] = root;
-    while( front < rear ) {
-        int u = q[front++];
-        for(int i = 1; i < maxd; ++i) {
-            f[u][i] = f[ f[u][i-1] ][ i-1 ];
-            if(f[u][i] == dummyroot) break;
+void LCA_PreProcess(LCA *lca, int root) {
+    lca->front = lca->rear = 0;
+    lca->f[root][0] = dummyroot;
+    lca->dep[root] = 0;
+    lca->q[lca->rear++] = root;
+
+    // 新增2：根结点有1种零食 w[root]
+    lca->fw[root][ lca->w[root] ] = 1;
+    while (lca->front < lca->rear) {
+        int u = lca->q[lca->front++];
+        for (int i = 1; i < maxd; ++i) {
+            lca->f[u][i] = lca->f[lca->f[u][i-1]][i-1];
+            if (lca->f[u][i] == dummyroot) break;
         }
-        for(int i = head[u]; i != -1; i = next[i]) {
-            int v = edge[i];
-            if(v == f[u][0]) {
+        for (int i = lca->head[u]; i != -1; i = lca->next[i]) {
+            int v = lca->edge[i];
+            if (v == lca->f[u][0]) {
                 continue;
             }
-            f[v][0] = u;
-            // 树上前缀和
+            lca->f[v][0] = u;
+            lca->dep[v] = lca->dep[u] + 1;
+            lca->q[lca->rear++] = v;
+
+            // 新增3：树上前缀和
             for(int j = 1; j <= 20; ++j) {
-                fw[v][j] = fw[u][j] + (w[v] == j ? 1 : 0);
+                lca->fw[v][j] = lca->fw[u][j] + (lca->w[v] == j ? 1 : 0);
             }
-            dep[v] = dep[u] + 1;
-            q[rear++] = v;
         }
     }
 }
 
-int LCA_Get(int u, int v) {
-    // 确保 u的深度 >= v的深度
-    if(dep[u] < dep[v]) {
-        return LCA_Get(v, u);
+int LCA_Get(LCA *lca, int u, int v) {
+    // 1、确保 u的深度 >= v的深度
+    if (lca->dep[u] < lca->dep[v]) {
+        return LCA_Get(lca, v, u); 
     }
-    // 将 u 和 v 调整到同一深度
-    for(int i = maxd-1; i >= 0; --i) {
-        if( dep[u] - (1<<i) >= dep[v] ) {
-            u = f[u][i];
+    // 2、将 u 和 v 调整到同一深度
+    for (int i = maxd - 1; i >= 0; --i) {
+        if (lca->dep[u] - (1 << i) >= lca->dep[v]) {
+            u = lca->f[u][i];
         }
     }
-    if(u == v) {
+    if (u == v) {
         return u;
     }
-    // 让 u 和 v 同时网上进行倍增
-    for(int i = maxd - 1; i >= 0; --i) {
-        if( f[u][i] != f[v][i] ) {
-            u = f[u][i];
-            v = f[v][i];
+    // 3、让 u 和 v 同时往上进行倍增
+    for (int i = maxd - 1; i >= 0; --i) {
+        if (lca->f[u][i] != lca->f[v][i]) {
+            u = lca->f[u][i];
+            v = lca->f[v][i];
         }
     }
-    return f[u][0];
+    return lca->f[u][0];
 }
+///////////////////////最近公共祖先(倍增算法)///////////////////////
+
+LCA lca;
 
 /*
            1
@@ -107,26 +120,29 @@ int main() {
     int n;
     int q;
     scanf("%d %d", &n, &q);
-    LCA_Init(n);
+    LCA_Initialize(&lca, n);
     for(int i = 1; i <= n; ++i) {
-        scanf("%d", &w[i]);
+        scanf( "%d", &(lca.w[i]) );
     }
     for(int i = 0; i < n-1; ++i) {
         int x, y;
         scanf("%d %d", &x, &y);
-        LCA_AddEdge(x, y);
+        LCA_AddEdge(&lca, x, y);
     }
-    LCA_PreProcess(1);
+    LCA_PreProcess(&lca, 1);
     while(q--) {
         int x, y;
         scanf("%d %d", &x, &y);
-        int u = LCA_Get(x, y);
+        int u = LCA_Get(&lca, x, y);
         int ans = 0;
-        // 枚举每一种零食，在 x->u 以及 y->u 的路径和上是否出现过
+        // 新增4：枚举每一种零食
+        // 在 x->u 以及 y->u 的路径和上是否出现过
         for(int j = 1; j <= 20; ++j) {
-            int val = (fw[x][j] - fw[u][j]) + (fw[y][j] - fw[u][j]) + (w[u] == j ? 1 : 0);
-            ans += (val > 0 ? 1 : 0);
-        } 
+            int val = (lca.fw[x][j] - lca.fw[u][j]) 
+            + (lca.fw[y][j] - lca.fw[u][j]) 
+            + (lca.w[u] == j ? 1 : 0);
+            ans += (val > 0);
+        }
         printf("%d\n", ans);
     }
     return 0;
